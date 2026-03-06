322a81fbc2d8855f9d3e0dde6a8efe033913bef1
YeetoNeto <netosisacoolone@gmail.com>
2026-03-06 12:49:18 -0500

    module 11 assignments

---
+++ module-11/Django Example/Django Example/Django_Example/urls.py
@@ -0,0 +1,16 @@
+"""
+Definition of urls for Django_Example.
+"""
+
+from datetime import datetime
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+from app import forms, views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include("app.Urls"))
+    
+]
